7312ea80a023ae53d6996188d072a52aaa2e5c00
Joey Langley <joemagnum1@hotmail.com>
2017-06-21 12:26:52 -0400

    database config stuff

--- databaseFiles/database_connections.php
+++ databaseFiles/database_connections.php
@@ -2,7 +2,4 @@
 // Connecting to database as mysqli_connect("hostname", "username", "password", "database name");
 //Local Dev Database
 $con = mysqli_connect("localhost", "root", "password", "storage_customer");
-
-// Live Database
-//$con = mysqli_connect("localhost", "joeylang_admin", "Alien999$", "joeylang_storage_app");
 ?>
